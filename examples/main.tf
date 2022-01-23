36589bc05f6fc0d8a707134512a2005082674a0c
Pujah Goviel <pujah.goviel@acc.co.nz>
2022-01-23 23:31:37 +1300

    removed service principal password module

--- examples/main.tf
+++ examples/main.tf
@@ -18,22 +18,4 @@ module "azure_ad_service_principal" {
     gallery    = true
   }
 
-  service_principal_id = "3d5c1675-fff2-49e5-b2eb-b6eff382926d"
-  display_name         = "My sp password"
-
-  # One may set an exact end date for the password using:
-  # end_date = "2023-01-01T01:02:03Z"
-
-  # One may use a relative end date using:
-  end_date_relative = "240h"
-
-  # Force re-creation of the password when a key value changes, for example when a time index expires
-  # rotate_when_changed = {
-  #   rotation = time_rotating.rotate.id
-  # }
-
-  # One may choose a start date for the password using:
-  # start_date = "2023-01-01T01:02:03Z"
-
-
 }
--- main.tf
+++ main.tf
@@ -33,11 +33,4 @@ resource "azuread_service_principal" "main" {
 }
 
 
-resource "azuread_service_principal_password" "main" {
-  service_principal_id = var.service_principal_id
-  display_name         = var.display_name
-  end_date             = var.end_date
-  end_date_relative    = var.end_date_relative
-  rotate_when_changed  = var.rotate_when_changed
-  start_date           = var.start_date
-}
+
